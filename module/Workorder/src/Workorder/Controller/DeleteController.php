4bb3aff2e549eac9c2fce0461014358008f55a6b
Jaimie Garner <admin@onlineauction.com>
2016-07-06 11:55:51 -0700

    Resolves #6 Complete Delete Work Order

--- module/Workorder/src/Workorder/Controller/DeleteController.php
+++ module/Workorder/src/Workorder/Controller/DeleteController.php
@@ -4,51 +4,108 @@ namespace Workorder\Controller;
 use Application\Controller\BaseController;
 use Client\Service\ClientServiceInterface;
 use Zend\View\Model\ViewModel;
+use Workorder\Service\WorkorderServiceInterface;
 
 class DeleteController extends BaseController
 {
+
     /**
      *
      * @var ClientServiceInterface
      */
     protected $clientService;
-    
+
     /**
      *
-     * @param ClientServiceInterface $clientService
+     * @var WorkorderServiceInterface
      */
-    public function __construct(ClientServiceInterface $clientService)
+    protected $workorderService;
+
+    /**
+     *
+     * @param ClientServiceInterface $clientService            
+     * @param WorkorderServiceInterface $workorderService            
+     */
+    public function __construct(ClientServiceInterface $clientService, WorkorderServiceInterface $workorderService)
     {
         $this->clientService = $clientService;
+        
+        $this->workorderService = $workorderService;
     }
-    
+
     /**
      *
      * {@inheritDoc}
+     *
      * @see \Zend\Mvc\Controller\AbstractActionController::indexAction()
      */
     public function indexAction()
     {
         $id = $this->params()->fromRoute('clientId');
-    
+        
+        $workorderId = $this->params()->fromRoute('workorderId');
+        
+        // get client entity
         $clientEntity = $this->clientService->get($id);
-    
-        if (! $clientEntity) {}
-    
+        
+        // validate we got a client
+        if (! $clientEntity) {
+            $this->flashMessenger()->addErrorMessage('Can not find client');
+            
+            return $this->redirect()->toRoute('client-index');
+        }
+        
+        $workorderEntity = $this->workorderService->get($workorderId);
+        
+        // validate we got a work order
+        if (! $workorderEntity) {
+            $this->flashMessenger()->addErrorMessage('Could not find work order');
+            
+            return $this->redirect()->toRoute('workorder-list', array(
+                'clientId' => $id
+            ));
+        }
+        
+        // request
+        $request = $this->getRequest();
+        
+        // if post
+        if ($request->isPost()) {
+            $del = $request->getPost('delete_confirmation', 'no');
+            
+            if ($del === 'yes') {
+                
+                $workorderEntity->setWorkorderStatus('Deleted');
+                
+                $this->workorderService->save($workorderEntity);
+                
+                $this->flashmessenger()->addSuccessMessage('The work order was deleted');
+                
+                return $this->redirect()->toRoute('workorder-list', array(
+                    'clientId' => $id
+                ));
+            }
+            
+            // return to view
+            return $this->redirect()->toRoute('client-view', array(
+                'clientId' => $id
+            ));
+        }
+        
+        // set up layout
         $this->layout()->setVariable('clientId', $id);
-    
+        
         $this->layout()->setVariable('pageTitle', 'Delete Work Order');
-    
+        
         $this->layout()->setVariable('pageSubTitle', $clientEntity->getClientName());
-    
-        $this->setHeadTitle($clientEntity->getClientName());
-    
+        
         $this->layout()->setVariable('activeMenuItem', 'client');
-    
+        
         // return View
         return new ViewModel(array(
             'clientEntity' => $clientEntity,
-            'clientId' => $id
+            'clientId' => $id,
+            'workorderEntity' => $workorderEntity
         ));
     }
 }
