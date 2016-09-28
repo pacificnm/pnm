b0d917e8af36cd40b581231d37b07e974eccf8e4
Jaimie Garner <pacificnm@gmail.com>
2016-09-28 06:42:42 -0700

    Resolves #93

--- module/Client/src/Client/Controller/UpdateController.php
+++ module/Client/src/Client/Controller/UpdateController.php
@@ -5,6 +5,10 @@ use Application\Controller\BaseController;
 use Zend\View\Model\ViewModel;
 use Client\Service\ClientServiceInterface;
 use Client\Form\ClientForm;
+use Location\Service\LocationServiceInterface;
+use Phone\Service\PhoneServiceInterface;
+use Location\Form\LocationForm;
+use Phone\Form\PhoneForm;
 class UpdateController extends BaseController
 {
     /**
@@ -14,6 +18,18 @@ class UpdateController extends BaseController
     protected $clientService;
     
     /**
+     *
+     * @var LocationServiceInterface
+     */
+    protected $locationService;
+    
+    /**
+     *
+     * @var PhoneServiceInterface
+     */
+    protected $phoneService;
+    
+    /**
      * 
      * @var ClientForm
      */
@@ -21,13 +37,38 @@ class UpdateController extends BaseController
     
     /**
      * 
-     * @param ClientServiceInterface $clientService
+     * @var PhoneForm
      */
-    public function __construct(ClientServiceInterface $clientService, ClientForm $clientForm)
+    protected $phoneForm;
+    
+    /**
+     * 
+     * @var LocationForm
+     */
+    protected $locationForm;
+    
+    /**
+     * 
+     * @param ClientServiceInterface $clientService
+     * @param LocationServiceInterface $locationService
+     * @param PhoneServiceInterface $phoneService
+     * @param ClientForm $clientForm
+     * @param LocationForm $locationForm
+     * @param PhoneForm $phoneForm
+     */
+    public function __construct(ClientServiceInterface $clientService, LocationServiceInterface $locationService, PhoneServiceInterface $phoneService, ClientForm $clientForm, LocationForm $locationForm, PhoneForm $phoneForm)
     {
         $this->clientService = $clientService;
         
+        $this->locationService = $locationService;
+        
+        $this->phoneService = $phoneService;
+        
         $this->clientForm = $clientForm;
+        
+        $this->locationForm = $locationForm;
+        
+        $this->phoneForm = $phoneForm;
     }
     
     /**
@@ -42,22 +83,52 @@ class UpdateController extends BaseController
         $clientEntity = $this->clientService->get($id);
         
         if (! $clientEntity) {
+            $this->flashmessenger()->addErrorMessage('Client was not found.');
             
+            return $this->redirect()->toRoute('client-index');
         }
+        
+        
         
         $request = $this->getRequest();
         
         // if we have a post
         if ($request->isPost()) {
+
             // get post
             $postData = $request->getPost();
             
+            $postData->locationType = 'Primary';
+            
+            $postData->locationStatus = 'Active';
+            
+            $postData->phoneType = 'Primary';
+            
             $this->clientForm->setData($postData);
             
-            if ($this->clientForm->isValid()) {
+            $this->phoneForm->setData($postData);
+            
+            $this->locationForm->setData($postData);
+            
+            // remove validation rule to save the location
+            $this->locationForm->getInputFilter()->remove('HasPrimary');
+            
+                        
+            // if valid
+            if ($this->clientForm->isValid() && $this->phoneForm->isValid() && $this->locationForm->isValid()) {
+               
                 $clientEntity = $this->clientForm->getData();
                 
+                $phoneEntity = $this->phoneForm->getData();
+                
+                
+                $locationEntity = $this->locationForm->getData();
+                
                 $clientEntity = $this->clientService->save($clientEntity);
+                
+                $phoneEntity = $this->phoneService->save($phoneEntity);
+                
+                $locationEntity = $this->locationService->save($locationEntity);
                 
                 $this->flashmessenger()->addSuccessMessage('The client was saved.');
                 
@@ -67,8 +138,14 @@ class UpdateController extends BaseController
             }
         }
         
+        // bind forms
         $this->clientForm->bind($clientEntity);
         
+        $this->locationForm->bind($clientEntity->getLocationEntity());
+        
+        $this->phoneForm->bind($clientEntity->getPhoneEntity());
+        
+        // set vars
         $this->layout()->setVariable('clientId', $id);
         
         $this->layout()->setVariable('pageTitle', 'Edit Client');
@@ -77,8 +154,11 @@ class UpdateController extends BaseController
         
         $this->setHeadTitle($clientEntity->getClientName());
         
+        // return view model
         return new ViewModel(array(
-            'form' => $this->clientForm
+            'form' => $this->clientForm,
+            'locationForm' => $this->locationForm,
+            'phoneForm' => $this->phoneForm
         ));
     }
 }
