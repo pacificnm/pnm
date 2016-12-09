d522ebacfb24096aeaaf59c5452de95fd54b9819
Jaimie Garner <pacificnm@gmail.com>
2016-12-09 14:01:43 -0800

    Adds work order totals to account home page

--- module/Install/Module.php
+++ module/Install/Module.php
@@ -1,8 +1,22 @@
 <?php
 namespace Install;
 
+use Zend\Console\Adapter\AdapterInterface as Console;
+
 class Module
 {
+    /**
+     *
+     * @param Console $console
+     * @return string[]|string[][]
+     */
+    public function getConsoleUsage(Console $console)
+    {
+        return array(
+            'install' => 'installs base application',
+        );
+    }
+    
     public function getConfig()
     {
         return include __DIR__ . '/config/module.config.php';
